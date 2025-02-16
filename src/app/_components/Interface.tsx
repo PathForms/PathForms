6758e4b375ffbe6c6d5264d8262f7c50726919dc
haochend413 <dinghaochen413@gmail.com>
2025-02-16 15:29:22 -0600

    minor fix

--- src/app/_components/Interface.tsx
+++ src/app/_components/Interface.tsx
@@ -20,66 +20,58 @@ const Interface = () => {
     //split the id
     const [x, y] = current_Node.split(",").map(Number);
     let next_Node_raw;
+    let i: number = 0;
     // Determine the new coordinate based on the direction
     switch (direction) {
       case "up":
         if (moves.length == 0 || moves[length - 1] != "down") {
           moves.push("up");
           next_Node_raw = [x, y - 100.0 / 2 ** (path.length - 1)];
+          i = 1;
         } else {
           moves.pop();
         }
 
         break;
       case "down":
-        if (moves.length == 0 || moves[length - 1] != "down") {
-          moves.push("up");
+        if (moves.length == 0 || moves[length - 1] != "up") {
+          moves.push("down");
+          i = 1;
           next_Node_raw = [x, y + 100.0 / 2 ** (path.length - 1)];
         } else {
           moves.pop();
         }
 
-        if (moves.length != 0) {
-          if (moves[length - 1] == "up") {
-            moves.pop();
-          } else {
-            moves.push("down");
-          }
-        } else {
-          moves.push("down");
-        }
-        next_Node_raw = [x, y + 100.0 / 2 ** (path.length - 1)];
         break;
       case "left":
-        if (moves.length != 0) {
-          if (moves[length - 1] == "right") {
-            moves.pop();
-          } else {
-            moves.push("left");
-          }
-        } else {
+        if (moves.length == 0 || moves[length - 1] != "right") {
           moves.push("left");
+          i = 1;
+          next_Node_raw = [x - 100.0 / 2 ** (path.length - 1), y];
+        } else {
+          moves.pop();
         }
-        next_Node_raw = [x - 100.0 / 2 ** (path.length - 1), y];
         break;
       case "right":
-        if (moves.length != 0) {
-          if (moves[length - 1] == "left") {
-            moves.pop();
-          } else {
-            moves.push("right");
-          }
-        } else {
+        if (moves.length == 0 || moves[length - 1] != "left") {
           moves.push("right");
+          i = 1;
+          next_Node_raw = [x + 100.0 / 2 ** (path.length - 1), y];
+        } else {
+          moves.pop();
         }
-        next_Node_raw = [x + 100.0 / 2 ** (path.length - 1), y];
         break;
       default:
         return;
     }
-    const next_Node = String(`${next_Node_raw[0]},${next_Node_raw[1]}`);
-    // Update the stack with the new coordinate
-    setPath([...path, next_Node]);
+
+    if (i == 1) {
+      const next_Node = String(`${next_Node_raw[0]},${next_Node_raw[1]}`);
+      // Update the stack with the new coordinate
+      setPath([...path, next_Node]);
+    } else {
+      setPath(path.slice(0, -1));
+    }
   };
 
   return (
