34ba1218d4f65f642ceba681e184ed67be5f7b80
haochend413 <dinghaochen413@gmail.com>
2025-03-17 15:54:04 -0500

    svg link fix

--- src/app/fsm/page.tsx
+++ src/app/fsm/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 function FSM() {
   return (
-    <Image src="/terminal_fsm.svg" alt="SVG Icon" width={100} height={100} />
+    <Image src="/terminal-fsm.svg" alt="SVG Icon" width={100} height={100} />
   );
 }
 
