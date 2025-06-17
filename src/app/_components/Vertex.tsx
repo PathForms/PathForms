00ecb260eccabac958e3c20d3c67fe83fd4d4d69
Maxwell <boyul5@illinois.edu>
2025-06-17 15:58:00 -0500

    style: make center vertex prominent

--- src/app/_components/Vertex.tsx
+++ src/app/_components/Vertex.tsx
@@ -12,17 +12,33 @@ interface VertexProps {
 
 const Vertex: React.FC<VertexProps> = ({ id, x, y, isActive }) => {
   //color logic
+  const isRoot = id === "0,0";
+
   let fillColor = "rgba(244, 252, 0, 0.14)";
+  let radius = 2;
+  let stroke = "transparent";
+  let strokeW = 0;
+
+  if (isRoot) {
+    fillColor = "#ffd700";
+    radius = 4;
+    stroke = "#ffffff";
+    strokeW = 0.8;
+  }
   if (isActive) {
-    fillColor = "rgb(255, 255, 255)";
+    fillColor = "#ffffff";
   }
 
+  const className = isRoot ? "root-vertex" : "";
   return (
     <circle
+      className = {className}
       cx={x}
       cy={y}
-      r={2}
+      r={radius}
       fill={fillColor}
+      stroke={stroke}
+      strokeWidth={strokeW}
       style={{ cursor: "pointer", transition: "fill 0.3s" }}
     />
   );
