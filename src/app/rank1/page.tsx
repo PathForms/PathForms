6d86236940f724ec97dd79953cbe5e2fd3da1fbc
Eunice Choi <eunicec4@illinois.edu>
2025-09-17 04:43:53 -0500

    add rank 1

---
+++ src/app/rank1/page.tsx
@@ -0,0 +1,7 @@
+export default function Rank1() {
+    return (
+        <div className="flex items-center justify-center h-screen bg-white">
+            <h1 className="text-4xl font-bold">Rank 1</h1>
+        </div>
+    );
+}
