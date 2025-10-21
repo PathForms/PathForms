22feda7427b6284b0051859e50f73d1bce54285a
Eunice Choi <eunicec4@illinois.edu>
2025-10-21 18:08:58 -0500

    landing page - rank2 file location adjusted

---
+++ src/app/rank2/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+
+import Interface from "../_components/Interface";
+
+const Rank2 = () => {
+  return (
+    <div className="cayley">
+      <Interface />
+    </div>
+  );
+};
+
+export default Rank2;
